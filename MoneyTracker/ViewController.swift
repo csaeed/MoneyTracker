5086f7eab22d605c6adb44c78e04293fabbd63cd
student5 <student3@jh-mba158-06.d214.org>
2017-04-28 09:49:29 -0500

    Added and then deleted delegate and data source

--- MoneyTracker/ViewController.swift
+++ MoneyTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
    
     var kodeKatz = true
     var int = 8
